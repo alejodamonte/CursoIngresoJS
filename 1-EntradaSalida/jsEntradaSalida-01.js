8459c9fbbf2ee7fe20e83fd7d7b2975f76834f39
alejo <alejodamonte17@gmail.com>
2022-02-01 19:26:57 -0300

    hecho

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,5 +1,7 @@
 /*
+
 Alejo Damonte DIV g
+
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,5 +1,7 @@
 /*
+
 Alejo Damonte DIV g
+
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
