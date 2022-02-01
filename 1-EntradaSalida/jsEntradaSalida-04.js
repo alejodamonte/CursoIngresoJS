c561db49f008e70672ca66f351a684c8e047ad53
alejo <alejodamonte17@gmail.com>
2022-02-01 20:41:52 -0300

    hecho

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,17 @@
 /* 
+
+Alejo Damonte DIV g
+
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+
+	nombre = prompt("ingrese su nombre");
+	
+	document.getElementById("txtIdNombre").value = nombre; 
+	
 }
 
