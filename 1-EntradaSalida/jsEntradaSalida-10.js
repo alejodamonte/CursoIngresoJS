610e215dda0ea38bb5c778936b9a34bb633d5b41
alejo <alejodamonte17@gmail.com>
2022-02-02 10:31:52 -0300

    hecho

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,16 @@
 /*
+
+Alejo Damonte DIV g
+
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importe
+	var resultado
+	importe = document.getElementById("txtIdImporte").value;
+	resultado = parseInt(importe) * 0.75;
+	document.getElementById("txtIdResultado").value = resultado;
 }
