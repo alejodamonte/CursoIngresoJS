b0f1b11e20b1e856250f6f8021c205e56ed0c961
alejo <alejodamonte17@gmail.com>
2022-02-02 18:33:42 -0300

    hecho

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -8,8 +8,8 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	var importe
-	var resultado
+	var importe;
+	var resultado;
 	importe = document.getElementById("txtIdImporte").value;
 	resultado = parseInt(importe) * 0.75;
 	document.getElementById("txtIdResultado").value = resultado;
