a64b7799ba8adf058ee74718890e1ecc94d2a9aa
alejo <alejodamonte17@gmail.com>
2022-02-03 21:42:34 -0300

    hecho

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,4 +1,8 @@
-/*2.	Para el departamento de Construcción:
+/*
+
+Alejo Damonte DIV g
+
+2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
@@ -21,8 +25,6 @@ function Rectangulo ()
     resultado = perimetroDelRectangulo * 3;
 
     alert("Necesita " + resultado + "m de alambre");
-
-
 }
 function Circulo () 
 {
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,36 @@
-/*3.	Para el departamento de Pinturas:
+/*
+
+Alejo Damonte DIV g
+
+3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
 function FahrenheitCentigrados () 
 {
-	
+	let temperatura
+    let resultado
+    
+    temperatura = document.getElementById("txtIdTemperatura").value;
+
+    temperatura = parseFloat(temperatura);
+
+    resultado = (temperatura - 32) / 1.8000;
+    
+    alert(temperatura + " Fahrenheit son " + resultado " centigrados");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    let temperatura
+    let resultado
+    
+    temperatura = document.getElementById("txtIdTemperatura").value;
+
+    temperatura = parseFloat(temperatura);
+
+    resultado = (temperatura * 1.8000) + 32;
+    
+    alert(temperatura + " Centigrados son " + resultado " Fahrenheit");
 }
+
